9773c2c44ce894cf98822df8ec39f6478aa79c8f
Adwaith Manoj <88626577+adwXD@users.noreply.github.com>
2021-08-08 22:29:06 +0530

    WhatsappMatic
    WE CAN CONTROL TIME
    Save Time

---
+++ AutoWhatsapp.py
@@ -0,0 +1,13 @@
+import pywhatkit
+pywhatkit.sendwhatmsg('(phno. Country Code)', '(message)', time use comma)
+
+
+
+#Developer adwXD
+# Use your country code and type ph.. to send mess.. to frnd..
+#Use time as Comma & 24h format
+#Eg:
+#time (12,5)
+#Take extension Pywhatkit from python OR pip.....:)
+
+# ENJOY XD  
